6f482248b7851023e7c35725c12d0d4d536d6298
happat <1316571084@qq.com>
2023-12-03 14:23:48 +0800

    tmp commit

--- include/fastnum/arithmetic.hpp
+++ include/fastnum/arithmetic.hpp
@@ -15,6 +15,17 @@ void sub_scaler(int M, int N, const float *A, int lda, const float B, float *C, 
 void mul_scaler(int M, int N, const float *A, int lda, const float B, float *C, int ldc);
 void div_scaler(int M, int N, const float *A, int lda, const float B, float *C, int ldc);
 
+
+void add(int M, int N, const double *A, int lda, const double* B, int ldb, double* C, int ldc);
+void sub(int M, int N, const double *A, int lda, const double* B, int ldb, double* C, int ldc);
+void mul(int M, int N, const double *A, int lda, const double* B, int ldb, double* C, int ldc);
+void div(int M, int N, const double *A, int lda, const double* B, int ldb, double* C, int ldc);
+
+void add_scaler(int M, int N, const double *A, int lda, const double B, double *C, int ldc);
+void sub_scaler(int M, int N, const double *A, int lda, const double B, double *C, int ldc);
+void mul_scaler(int M, int N, const double *A, int lda, const double B, double *C, int ldc);
+void div_scaler(int M, int N, const double *A, int lda, const double B, double *C, int ldc);
+
 void fma(int M, int N, const float *A, int lda, const float *B, int ldb, const float *C, int ldc, float *D, int ldd);
 
 }  // namespace cpu
--- source/arithemetic.cpp
+++ source/arithemetic.cpp
@@ -67,16 +67,6 @@ void div(int M, int N, const float* A, int lda, const float* B, int ldb, float* 
 void add_scaler(int M, int N, const float* A, int lda, const float B, float* C, int ldc) {
     int m = 0;
 
-    for (; m < M - 7; m += 8) {
-        kernel::add_scaler(C + (m + 0) * ldc, A + (m + 0) * lda, B, N);
-        kernel::add_scaler(C + (m + 1) * ldc, A + (m + 1) * lda, B, N);
-        kernel::add_scaler(C + (m + 2) * ldc, A + (m + 2) * lda, B, N);
-        kernel::add_scaler(C + (m + 3) * ldc, A + (m + 3) * lda, B, N);
-        kernel::add_scaler(C + (m + 4) * ldc, A + (m + 4) * lda, B, N);
-        kernel::add_scaler(C + (m + 5) * ldc, A + (m + 5) * lda, B, N);
-        kernel::add_scaler(C + (m + 6) * ldc, A + (m + 6) * lda, B, N);
-        kernel::add_scaler(C + (m + 7) * ldc, A + (m + 7) * lda, B, N);
-    }
     for (; m < M - 3; m += 4) {
         kernel::add_scaler(C + (m + 0) * ldc, A + (m + 0) * lda, B, N);
         kernel::add_scaler(C + (m + 1) * ldc, A + (m + 1) * lda, B, N);
@@ -91,16 +81,6 @@ void add_scaler(int M, int N, const float* A, int lda, const float B, float* C, 
 void sub_scaler(int M, int N, const float* A, int lda, const float B, float* C, int ldc) {
     int m = 0;
 
-    for (; m < M - 7; m += 8) {
-        kernel::sub_scaler(C + (m + 0) * ldc, A + (m + 0) * lda, B, N);
-        kernel::sub_scaler(C + (m + 1) * ldc, A + (m + 1) * lda, B, N);
-        kernel::sub_scaler(C + (m + 2) * ldc, A + (m + 2) * lda, B, N);
-        kernel::sub_scaler(C + (m + 3) * ldc, A + (m + 3) * lda, B, N);
-        kernel::sub_scaler(C + (m + 4) * ldc, A + (m + 4) * lda, B, N);
-        kernel::sub_scaler(C + (m + 5) * ldc, A + (m + 5) * lda, B, N);
-        kernel::sub_scaler(C + (m + 6) * ldc, A + (m + 6) * lda, B, N);
-        kernel::sub_scaler(C + (m + 7) * ldc, A + (m + 7) * lda, B, N);
-    }
     for (; m < M - 3; m += 4) {
         kernel::sub_scaler(C + (m + 0) * ldc, A + (m + 0) * lda, B, N);
         kernel::sub_scaler(C + (m + 1) * ldc, A + (m + 1) * lda, B, N);
@@ -131,16 +111,6 @@ void mul_scaler(int M, int N, const float* A, int lda, const float B,  float* C,
 void div_scaler(int M, int N, const float* A, int lda, const float B, float* C, int ldc) {
     int m = 0;
 
-    for (; m < M - 7; m += 8) {
-        kernel::div_scaler(C + (m + 0) * ldc, A + (m + 0) * lda, B, N);
-        kernel::div_scaler(C + (m + 1) * ldc, A + (m + 1) * lda, B, N);
-        kernel::div_scaler(C + (m + 2) * ldc, A + (m + 2) * lda, B, N);
-        kernel::div_scaler(C + (m + 3) * ldc, A + (m + 3) * lda, B, N);
-        kernel::div_scaler(C + (m + 4) * ldc, A + (m + 4) * lda, B, N);
-        kernel::div_scaler(C + (m + 5) * ldc, A + (m + 5) * lda, B, N);
-        kernel::div_scaler(C + (m + 6) * ldc, A + (m + 6) * lda, B, N);
-        kernel::div_scaler(C + (m + 7) * ldc, A + (m + 7) * lda, B, N);
-    }
     for (; m < M - 3; m += 4) {
         kernel::div_scaler(C + (m + 0) * ldc, A + (m + 0) * lda, B, N);
         kernel::div_scaler(C + (m + 1) * ldc, A + (m + 1) * lda, B, N);
@@ -151,6 +121,122 @@ void div_scaler(int M, int N, const float* A, int lda, const float B, float* C, 
         kernel::div_scaler(C + (m + 0) * ldc, A + (m + 0) * lda, B, N);
     }
 }
+
+
+void add(int M, int N, const double *A, int lda, const double* B, int ldb, double* C, int ldc) {
+    int m = 0;
+
+    for (; m < M - 3; m += 4) {
+        kernel::add(C + (m + 0) * ldc, A + (m + 0) * lda, B + (m + 0) * ldb, N);
+        kernel::add(C + (m + 1) * ldc, A + (m + 1) * lda, B + (m + 1) * ldb, N);
+        kernel::add(C + (m + 2) * ldc, A + (m + 2) * lda, B + (m + 2) * ldb, N);
+        kernel::add(C + (m + 3) * ldc, A + (m + 3) * lda, B + (m + 3) * ldb, N);
+    }
+
+    for(; m<M; ++m) {
+        kernel::add(C + (m + 0) * ldc, A + (m + 0) * lda, B + (m + 0) * ldb, N);
+    }
+}
+
+void sub(int M, int N, const double *A, int lda, const double* B, int ldb, double* C, int ldc) {
+    int m = 0;
+
+    for (; m < M - 3; m += 4) {
+        kernel::sub(C + (m + 0) * ldc, A + (m + 0) * lda, B + (m + 0) * ldb, N);
+        kernel::sub(C + (m + 1) * ldc, A + (m + 1) * lda, B + (m + 1) * ldb, N);
+        kernel::sub(C + (m + 2) * ldc, A + (m + 2) * lda, B + (m + 2) * ldb, N);
+        kernel::add(C + (m + 3) * ldc, A + (m + 3) * lda, B + (m + 3) * ldb, N);
+    }
+
+    for(; m<M; ++m) {
+        kernel::sub(C + (m + 0) * ldc, A + (m + 0) * lda, B + (m + 0) * ldb, N);
+    }
+}
+
+void mul(int M, int N, const double *A, int lda, const double* B, int ldb, double* C, int ldc) {
+    int m = 0;
+
+    for (; m < M - 3; m += 4) {
+        kernel::mul(C + (m + 0) * ldc, A + (m + 0) * lda, B + (m + 0) * ldb, N);
+        kernel::mul(C + (m + 1) * ldc, A + (m + 1) * lda, B + (m + 1) * ldb, N);
+        kernel::mul(C + (m + 2) * ldc, A + (m + 2) * lda, B + (m + 2) * ldb, N);
+        kernel::mul(C + (m + 3) * ldc, A + (m + 3) * lda, B + (m + 3) * ldb, N);
+    }
+
+    for(; m<M; ++m) {
+        kernel::mul(C + (m + 0) * ldc, A + (m + 0) * lda, B + (m + 0) * ldb, N);
+    }
+}
+void div(int M, int N, const double *A, int lda, const double* B, int ldb, double* C, int ldc) {
+    int m = 0;
+
+    for (; m < M - 3; m += 4) {
+        kernel::div(C + (m + 0) * ldc, A + (m + 0) * lda, B + (m + 0) * ldb, N);
+        kernel::div(C + (m + 1) * ldc, A + (m + 1) * lda, B + (m + 1) * ldb, N);
+        kernel::div(C + (m + 2) * ldc, A + (m + 2) * lda, B + (m + 2) * ldb, N);
+        kernel::div(C + (m + 3) * ldc, A + (m + 3) * lda, B + (m + 3) * ldb, N);
+    }
+
+    for(; m<M; ++m) {
+        kernel::div(C + (m + 0) * ldc, A + (m + 0) * lda, B + (m + 0) * ldb, N);
+    }
+}
+
+void add_scaler(int M, int N, const double *A, int lda, const double B, double *C, int ldc) {
+    int m = 0;
+
+    for (; m < M - 3; m += 4) {
+        kernel::add_scaler(C + (m + 0) * ldc, A + (m + 0) * lda, B, N);
+        kernel::add_scaler(C + (m + 1) * ldc, A + (m + 1) * lda, B, N);
+        kernel::add_scaler(C + (m + 2) * ldc, A + (m + 2) * lda, B, N);
+        kernel::add_scaler(C + (m + 3) * ldc, A + (m + 3) * lda, B, N);
+    }
+    for(; m<M; ++m) {
+        kernel::add_scaler(C + (m + 0) * ldc, A + (m + 0) * lda, B, N);
+    }
+}
+void sub_scaler(int M, int N, const double *A, int lda, const double B, double *C, int ldc) {
+    int m = 0;
+
+    for (; m < M - 3; m += 4) {
+        kernel::sub_scaler(C + (m + 0) * ldc, A + (m + 0) * lda, B, N);
+        kernel::sub_scaler(C + (m + 1) * ldc, A + (m + 1) * lda, B, N);
+        kernel::sub_scaler(C + (m + 2) * ldc, A + (m + 2) * lda, B, N);
+        kernel::sub_scaler(C + (m + 3) * ldc, A + (m + 3) * lda, B, N);
+    }
+    for(; m<M; ++m) {
+        kernel::sub_scaler(C + (m + 0) * ldc, A + (m + 0) * lda, B, N);
+    }
+}
+void mul_scaler(int M, int N, const double *A, int lda, const double B, double *C, int ldc) {
+    if(A == C && lda == ldc && B == 1) return;
+
+    int m = 0;
+    for (; m < M - 3; m += 4) {
+        kernel::mul_scaler(C + (m + 0) * ldc, A + (m + 0) * lda, B, N);
+        kernel::mul_scaler(C + (m + 1) * ldc, A + (m + 1) * lda, B, N);
+        kernel::mul_scaler(C + (m + 2) * ldc, A + (m + 2) * lda, B, N);
+        kernel::mul_scaler(C + (m + 3) * ldc, A + (m + 3) * lda, B, N);
+    }
+    for(; m<M; ++m) {
+        kernel::mul_scaler(C + (m + 0) * ldc, A + (m + 0) * lda, B, N);
+    }
+}
+
+void div_scaler(int M, int N, const double *A, int lda, const double B, double *C, int ldc) {
+    int m = 0;
+    
+    for (; m < M - 3; m += 4) {
+        kernel::div_scaler(C + (m + 0) * ldc, A + (m + 0) * lda, B, N);
+        kernel::div_scaler(C + (m + 1) * ldc, A + (m + 1) * lda, B, N);
+        kernel::div_scaler(C + (m + 2) * ldc, A + (m + 2) * lda, B, N);
+        kernel::div_scaler(C + (m + 3) * ldc, A + (m + 3) * lda, B, N);
+    }
+    for(; m<M; ++m) {
+        kernel::div_scaler(C + (m + 0) * ldc, A + (m + 0) * lda, B, N);
+    }
+}
+
 
 void fma(int M, int N, const float* A, int lda, const float* B, int ldb, const float* C, int ldc, float* D, int ldd) {
     for (int m = 0; m < M; ++m) {
